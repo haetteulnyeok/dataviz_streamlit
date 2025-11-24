2cc2155ffe94c4b04d394fe09de16642040c2968
haetteulnyeok <roborobo0528@gmail.com>
2025-11-24 19:46:51 +0900

    Delete pages/3_🎠레이아웃.py
    ㅇㅁㄴㄹ

--- pages/3_🎠레이아웃.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st 
-'### : orange[컬럼: st.columns()]'
-col_1, col_2, col_3 = st.columns([1,2,1])
-
-with col_1:
-    st.write('##1번 컬럼')
-    st.checkbox('이것은 1번 컬럼에 속한 체크박스 1')
-    st.checkbox('이것은 1번 컬럼에 속한 체크박스 2')
